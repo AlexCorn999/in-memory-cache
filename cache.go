5048ceba7aaf91cd88a794862615b8eb7d6e4c5f
AlexCorn999 <alexcorn999@list.ru>
2023-03-17 14:11:21 +0300

    added time.Expiration v1.0.2

--- cache.go
+++ cache.go
@@ -2,50 +2,128 @@ package inMemoryCache
 
 import (
 	"errors"
-	"fmt"
+	"sync"
 	"time"
 )
 
 type Value struct {
-	Value any
-	Ttl   *time.Duration
+	Value             any
+	Created           time.Time
+	DefaultExpiration int64
 }
 
 type Cache struct {
-	Storage map[string]*Value
+	sync.RWMutex
+	cleanupInterval time.Duration
+	storage         map[string]*Value
 }
 
 func New() *Cache {
-	return &Cache{make(map[string]*Value)}
-}
 
-func (c *Cache) Set(key string, value any, ttl time.Duration) {
-	v := Value{
-		Value: value,
-		Ttl:   &ttl,
+	db := &Cache{
+		storage:         make(map[string]*Value),
+		cleanupInterval: time.Second * 2,
 	}
 
-	c.Storage[key] = &v
+	db.StartGC()
+
+	return db
 }
 
-func (c *Cache) Get(key string) (any, error) {
+// установка значений по ключу
+func (c *Cache) Set(key string, value any, ttl time.Duration) {
 
-	value, ok := c.Storage[key]
+	c.Lock()
+
+	defer c.Unlock()
+
+	v := Value{
+		Value:             value,
+		Created:           time.Now(),
+		DefaultExpiration: time.Now().Add(ttl).UnixNano(),
+	}
+
+	c.storage[key] = &v
+}
+
+// получение значения по ключу
+func (db *Cache) Get(key string) (any, error) {
+
+	db.RLock()
+
+	defer db.RUnlock()
+
+	value, ok := db.storage[key]
 	if !ok {
 		return nil, errors.New("value not found")
 	}
 
-	fmt.Println(*value.Ttl)
-
 	return value.Value, nil
 }
 
-func (c *Cache) Delete(key string) error {
-	_, ok := c.Storage[key]
-	if !ok {
+// удаление ключа
+func (db *Cache) Delete(key string) error {
+
+	db.Lock()
+
+	defer db.Unlock()
+
+	if _, ok := db.storage[key]; !ok {
 		return errors.New("value not found")
 	}
 
-	delete(c.Storage, key)
+	delete(db.storage, key)
 	return nil
+}
+
+func (db *Cache) StartGC() {
+	go db.GC()
+}
+
+func (db *Cache) GC() {
+
+	for {
+		// ожидаем время установленное в cleanupInterval
+		<-time.After(db.cleanupInterval)
+
+		if db.storage == nil {
+			return
+		}
+
+		// Ищем элементы с истекшим временем жизни и удаляем из хранилища
+		if keys := db.expiredKeys(); len(keys) != 0 {
+			db.clearItems(keys)
+
+		}
+
+	}
+
+}
+
+// expiredKeys возвращает список "просроченных" ключей
+func (db *Cache) expiredKeys() (keys []string) {
+
+	db.RLock()
+
+	defer db.RUnlock()
+
+	for k, i := range db.storage {
+		if time.Now().UnixNano() > i.DefaultExpiration && i.DefaultExpiration > 0 {
+			keys = append(keys, k)
+		}
+	}
+
+	return
+}
+
+// clearItems удаляет ключи из переданного списка, в нашем случае "просроченные"
+func (db *Cache) clearItems(keys []string) {
+
+	db.Lock()
+
+	defer db.Unlock()
+
+	for _, k := range keys {
+		delete(db.storage, k)
+	}
 }
